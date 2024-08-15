2e0e6e4ec949453deaa7b46a398eba5ced1a0341
MatthewNoyce <131261051+MatthewNoyce@users.noreply.github.com>
2024-08-15 12:59:23 +0100

    Add files via upload
    Fixed issue with num_workers and added class_id NN

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from torchvision.io import read_image
 import pandas as pd
 import csv
 import os 
+
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
@@ -34,7 +35,52 @@ if __name__ == '__main__':
             return image, label, bbox_xxyy
         
     train_dataset = CarsDataset(data)
-    train_datloader = DataLoader(train_dataset, batch_size=16, num_workers=4, shuffle=False)
-    for i in enumerate(train_datloader):
-        print(len(i))
-        
+    train_datloader = DataLoader(train_dataset, batch_size=16, shuffle=False)
+    #Train_dataloader gives classid, bbox coords
+
+
+
+#Neural network class for class_id
+    class Cnn(nn.Module):
+        def __init__(self):
+            super(Cnn,self).__init__()
+    
+            self.layer1 = nn.Sequential(
+                nn.Conv2d(3,16,kernel_size=3, padding=0,stride=2),
+                nn.BatchNorm2d(16),
+                nn.ReLU(),
+                nn.MaxPool2d(2)
+            )
+            
+            self.layer2 = nn.Sequential(
+                nn.Conv2d(16,32, kernel_size=3, padding=0, stride=2),
+                nn.BatchNorm2d(32),
+                nn.ReLU(),
+                nn.MaxPool2d(2)
+                )
+            
+            self.layer3 = nn.Sequential(
+                nn.Conv2d(32,64, kernel_size=3, padding=0, stride=2),
+                nn.BatchNorm2d(64),
+                nn.ReLU(),
+                nn.MaxPool2d(2)
+            )
+            
+            
+            self.fc1 = nn.Linear(2304,512)
+            self.fc2 = nn.Linear(512,512)
+            self.fc3 = nn.Linear(512,5)
+            self.tanh = nn.Tanh()
+
+        def forward(self,x):
+
+            out = self.layer1(x)
+            out = self.layer2(out)
+            out = self.layer3(out)
+            out = out.view(out.size(0),-1)
+            out = self.tanh(self.fc1(out))
+            out = self.tanh(self.fc2(out))
+            out = self.fc3(out)
+
+            return out
+
